8aec724a9b4f8c85d4f363ee83fc27d63589e110
sagar-aryal <sagarandaryal@gmail.com>
2022-11-14 20:48:15 +0200

    prototype concept in js

--- src/day1.js
+++ src/day1.js
@@ -1,11 +1,13 @@
 // Todays Topics
 
 /* 
-1.Prototyping
+1. Prototyping
+2. Inheritence
  */
 
 function Person(name) {
   this.name = name;
+  this.nationality1 = "English";
 
   this.getName = function () {
     return this.name;
@@ -24,6 +26,15 @@ person1.age = 32;
 console.log(person1.name, person1.age);
 console.log(person2.name, person2.age);
 console.log(person1.getName());
+
+// We cannot add a new property directly to an existing object constructor
+Person.nationality = "Finnish";
+console.log(person1.nationality);
+
+// we can do this by two ways, either adding a new property to a constructor or using prototype key word
+Person.prototype.nationality2 = "Finnish";
+console.log(person1.nationality1);
+console.log(person1.nationality2);
 
 const arr = new Array(1, 2, 3);
 console.log(arr);
