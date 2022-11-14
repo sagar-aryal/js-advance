24940992185c3acb5a918f8b5fd35e01bdc9684b
sagar-aryal <sagarandaryal@gmail.com>
2022-11-14 20:17:53 +0200

    prototyping concept in js

--- src/day1.js
+++ src/day1.js
@@ -1,10 +1,29 @@
 // Todays Topics
 
+/* 
+1.Prototyping
+ */
+
 function Person(name) {
   this.name = name;
+
+  this.getName = function () {
+    return this.name;
+  };
 }
 
 const person1 = new Person("Sushil");
-const person2 = new Person("John");
+// const person2 = new Person("John");
 
-console.log(person1, person2);
+const person2 = person1;
+
+person2.name = "Alex";
+person1.age = 32;
+
+// Both person1 and person2 values will be changed
+console.log(person1.name, person1.age);
+console.log(person2.name, person2.age);
+console.log(person1.getName());
+
+const arr = new Array(1, 2, 3);
+console.log(arr);
